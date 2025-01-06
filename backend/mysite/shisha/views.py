a2e6ac9a15b6dad5e3c44aaf6ca4bef95160ac70
flatp <flat.pp057@icloud.com>
2025-01-07 03:49:34 +0900

    api modify

--- backend/mysite/shisha/views.py
+++ backend/mysite/shisha/views.py
@@ -51,23 +51,43 @@ class UserViewSet(viewsets.ModelViewSet):
         # 更新時も同様にハッシュ化を行う
         serializer.save()
 
+    # @action(detail=True, methods=['post'], url_path='follow')
+    # def follow_user(self, request, pk=None):
+    #     target_user_id = request.data.get('target_user_id')
+    #     user = self.get_object()
+    #     target_user = User.objects.get(pk=target_user_id)
+        
+    #     if target_user not in user.following.all():
+    #         user.following.add(target_user)
+    #         return Response({"detail": f"{target_user.name}をフォローしました。"}, status=status.HTTP_200_OK)
+    #     return Response({"detail": "既にフォローしています。"}, status=status.HTTP_400_BAD_REQUEST)
     @action(detail=True, methods=['post'], url_path='follow')
     def follow_user(self, request, pk=None):
-        target_user_id = request.data.get('target_user_id')
-        user = self.get_object()
-        target_user = User.objects.get(pk=target_user_id)
-        
+        user = request.user
+        target_user = self.get_object()
+
+        if user == target_user:
+            return Response({"detail": "自分自身をフォローすることはできません。"}, status=status.HTTP_400_BAD_REQUEST)
         if target_user not in user.following.all():
             user.following.add(target_user)
             return Response({"detail": f"{target_user.name}をフォローしました。"}, status=status.HTTP_200_OK)
         return Response({"detail": "既にフォローしています。"}, status=status.HTTP_400_BAD_REQUEST)
     
+    # @action(detail=True, methods=['post'], url_path='unfollow')
+    # def unfollow_user(self, request, pk=None):
+    #     target_user_id = request.data.get('target_user_id')
+    #     user = self.get_object()
+    #     target_user = User.objects.get(pk=target_user_id)
+        
+    #     if target_user in user.following.all():
+    #         user.following.remove(target_user)
+    #         return Response({"detail": f"{target_user.name}のフォローを取り消しました。"}, status=status.HTTP_200_OK)
+    #     return Response({"detail": "フォローしていません。"}, status=status.HTTP_400_BAD_REQUEST)
     @action(detail=True, methods=['post'], url_path='unfollow')
     def unfollow_user(self, request, pk=None):
-        target_user_id = request.data.get('target_user_id')
-        user = self.get_object()
-        target_user = User.objects.get(pk=target_user_id)
-        
+        user = request.user
+        target_user = self.get_object()
+
         if target_user in user.following.all():
             user.following.remove(target_user)
             return Response({"detail": f"{target_user.name}のフォローを取り消しました。"}, status=status.HTTP_200_OK)
@@ -84,15 +104,29 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-id')
     serializer_class = PostSerializer
 
-    @action(detail=False, methods=['get'], url_path='user-posts/(?P<user_id>[^/.]+)')
-    def user_posts(self, request, user_id=None):
-        posts = Post.objects.filter(user__id=user_id).order_by('-id')
+    # @action(detail=False, methods=['get'], url_path='user-posts/(?P<user_id>[^/.]+)')
+    # def user_posts(self, request, user_id=None):
+    #     posts = Post.objects.filter(user__id=user_id).order_by('-id')
+    #     serializer = self.get_serializer(posts, many=True)
+    #     return Response(serializer.data)
+    @action(detail=False, methods=['get'], url_path='user-posts')
+    def user_posts(self, request):
+        user = request.user
+        posts = Post.objects.filter(user__id=user.id).order_by('-id')
         serializer = self.get_serializer(posts, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['get'], url_path='following-posts/(?P<user_id>[^/.]+)')
-    def following_posts(self, request, user_id=None):
-        user = User.objects.get(pk=user_id)
+    # @action(detail=False, methods=['get'], url_path='following-posts/(?P<user_id>[^/.]+)')
+    # def following_posts(self, request, user_id=None):
+    #     user = User.objects.get(pk=user_id)
+    #     following_users = user.following.all()
+    #     all_users = list(following_users) + [user]
+    #     posts = Post.objects.filter(user__in=all_users).order_by('-id')
+    #     serializer = self.get_serializer(posts, many=True)
+    #     return Response(serializer.data)
+    @action(detail=False, methods=['get'], url_path='following-posts')
+    def following_posts(self, request):
+        user = request.user
         following_users = user.following.all()
         all_users = list(following_users) + [user]
         posts = Post.objects.filter(user__in=all_users).order_by('-id')
@@ -101,9 +135,10 @@ class PostViewSet(viewsets.ModelViewSet):
     
     @action(detail=True, methods=['post'], url_path='like')
     def like_post(self, request, pk=None):
-        user_id = request.data.get('user_id')
+        # user_id = request.data.get('user_id')
+        user = request.user
         post = self.get_object()
-        user = User.objects.get(pk=user_id)
+        user = User.objects.get(pk=user.id)
         
         if user not in post.liked.all():
             post.liked.add(user)
@@ -112,18 +147,25 @@ class PostViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['post'], url_path='unlike')
     def unlike_post(self, request, pk=None):
-        user_id = request.data.get('user_id')
+        # user_id = request.data.get('user_id')
+        user = request.user
         post = self.get_object()
-        user = User.objects.get(pk=user_id)
+        user = User.objects.get(pk=user.id)
         
         if user in post.liked.all():
             post.liked.remove(user)
             return Response({"detail": "いいねを取り消しました。"}, status=status.HTTP_200_OK)
         return Response({"detail": "いいねしていません。"}, status=status.HTTP_400_BAD_REQUEST)
     
-    @action(detail=False, methods=['get'], url_path='liked-posts/(?P<user_id>[^/.]+)')
-    def liked_posts(self, request, user_id=None):
-        posts = Post.objects.filter(liked__id=user_id).order_by('-id')
+    # @action(detail=False, methods=['get'], url_path='liked-posts/(?P<user_id>[^/.]+)')
+    # def liked_posts(self, request, user_id=None):
+    #     posts = Post.objects.filter(liked__id=user_id).order_by('-id')
+    #     serializer = self.get_serializer(posts, many=True)
+    #     return Response(serializer.data)
+    @action(detail=False, methods=['get'], url_path='liked-posts')
+    def liked_posts(self, request):
+        user = request.user
+        posts = Post.objects.filter(liked__id=user.id).order_by('-id')
         serializer = self.get_serializer(posts, many=True)
         return Response(serializer.data)
     
